b48a2b6a2f753384a6404f0d44977ff36bf86123
Andy Bullivent <andy.bullivent@collabco.co.uk>
2024-06-10 21:24:34 +0100

    Just handling loading of the posts a little better

--- ADB.Blogger.Client/src/app/display-post-list/display-post-list.component.ts
+++ ADB.Blogger.Client/src/app/display-post-list/display-post-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BloggerApiService } from '../services/blogger-api.service';
 import { Post } from '../models/post.model';
 import { StateResult } from '../models/state-result.model';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-display-post-list',
@@ -11,37 +12,52 @@ import { StateResult } from '../models/state-result.model';
 export class DisplayPostListComponent {
 
 
-menuOpen: boolean = false;
-viewTile: boolean = true;
-emptyAuthor = {
-  firstName: '--',
-  surname: '--',
-}
-emptyPost = {
-  id: '',
-  body:'',
-  createdBy: this.emptyAuthor,
-  actions: [],
-  title : 'No title available',
-  createdAt : new Date()
-}
-highlightedPost: Post = this.emptyPost;
+  menuOpen: boolean = false;
+  viewTile: boolean = true;
+  loading: boolean = false;
+  error: boolean = false;
 
-result: StateResult<Post[]> = {
-  actions: [],
-  data: []
-};
+  emptyAuthor = {
+    firstName: '--',
+    surname: '--',
+  }
+
+  emptyPost = {
+    id: '',
+    body:'',
+    createdBy: this.emptyAuthor,
+    actions: [],
+    title : 'No title available',
+    createdAt : new Date()
+  }
+
+  highlightedPost: Post = this.emptyPost;
+
+  result: StateResult<Post[]> = {
+    actions: [],
+    data: []
+  };
+  
   
 
   constructor(private bloggerApiService: BloggerApiService) {
-
+    this.loading = true;
     this.bloggerApiService.getPosts()
-      .subscribe(data => {
-        if (data && data.data && data.data.length > 0) {
-          this.highlightedPost = data.data[0];                 
+      .pipe(finalize(() => this.loading = false))
+      .subscribe({
+        next: data => {
+          if (data && data.data && data.data.length > 0) {
+            this.highlightedPost = data.data[0];                 
+          }
+          this.result = data;
+        },
+        error: err => {
+          console.error("An error occurred");
+          console.log(err);
+          this.error = true;
         }
-        this.result = data;
-    });
+    }
+    );
     
   }
 
